566efec63874236b935176397e2560baacb6c73d
GTK <gtkgcp21@gmail.com>
2026-06-23 11:46:14 +0530

    dns creation updated

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,14 +1,20 @@
-data "google_dns_managed_zone" "sarthi" {
-  name    = var.dns_managed_zone_name
-  project = var.project_id
+resource "google_dns_managed_zone" "sarthi" {
+  name        = var.dns_managed_zone_name      
+  dns_name    = "${var.environment}.${var.domain_name}."  
+  description = "Managed zone for Sarthi ${var.environment} environment"
+  project     = var.project_id
+
+  dnssec_config {
+    state = "off"
+  }
 }
 
 resource "google_dns_record_set" "keycloak" {
   project      = var.project_id
-  managed_zone = data.google_dns_managed_zone.sarthi.name
+  managed_zone = google_dns_managed_zone.sarthi.name
   name         = "${local.keycloak_hostname}."
-  type         = var.dns_record_type
-  ttl          = var.dns_record_ttl
+  type         = var.dns_record_type            
+  ttl          = var.dns_record_ttl            
   rrdatas      = [module.load_balancer.lb_ip_address]
 
   depends_on = [module.load_balancer]
